ad1fc442fe6a4ced1fda85efd81d38d52413a99a
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-06-23 08:47:48 -0700

    connect database

--- data/config.py
+++ data/config.py
@@ -5,3 +5,8 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
+
+DB_NAME = env.str("DB_NAME")
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASSWORD")
+DB_HOST = env.str("DB_HOST")
---
+++ utils/db_api/psql.py
@@ -0,0 +1,56 @@
+from typing import Union
+
+import asyncpg
+
+from asyncpg import Connection
+from asyncpg.pool import Pool
+
+from data import config
+
+
+class Database:
+    def __init__(self):
+        self.pool: Union[Pool, None] = None
+
+    async def create(self):
+        self.pool = await asyncpg.create_pool(
+            database=config.DB_NAME,
+            user=config.DB_USER,
+            password=config.DB_PASS,
+            host=config.DB_HOST
+        )
+
+    async def execute(
+            self, command, *args,
+            fetch: bool = False,
+            fetchval: bool = False,
+            fetchrow: bool = False,
+            execute: bool = False
+    ):
+        async with self.pool.acquire() as connection:
+            connection: Connection
+            async with connection.transaction():
+                if fetch:
+                    result = await connection.fetch(command, *args)
+                elif fetchval:
+                    result = await connection.fetchval(command, *args)
+                elif fetchrow:
+                    result = await connection.fetchrow(command, *args)
+                elif execute:
+                    result = await connection.execute(command, *args)
+            return result
+
+    @staticmethod
+    def format_args(sql, parameters: dict):
+        sql += " AND ".join(
+            [f"{item} = ${num}" for num, item in enumerate(parameters.keys(), start=1)]
+        )
+        return sql, tuple(parameters.values())
+
+    async def get_user_id(self, telegram_id):
+        sql = 'SELECT id FROM accounts_user WHERE telegram_id = $1'
+        return await self.execute(sql, str(telegram_id), fetchval=True)
+
+    async def count_users(self):
+        sql = "SELECT COUNT(*) FROM accounts_user"
+        return await self.execute(sql, fetchval=True)
