ca0878442071b19c574a8a4158431dcb297f1569
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-06-25 22:58:29 -0700

    update start and echo handlers

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,10 +1,27 @@
-from aiogram import types
+from aiogram.types import Message
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from loader import dp, db
+
+from data.words import get_word as _
+from keyboards.default import buttons
+from states.auth import UserRegisterState
 
 
-# Echo bot
-@dp.message_handler(state=None)
-async def bot_echo(message: types.Message):
-    print(message.text)
-    pass
+@dp.message_handler(CommandStart(), state=None)
+async def bot_echo(message: Message, state: FSMContext):
+    user_id = await db.get_user_id(message.from_user.id)
+    if user_id is not None:
+        await state.finish()
+        await message.answer(   
+            f'Aniqlanmagan buyruq !',
+            reply_markup=buttons([_('add_device'), _('my_devices')])
+        )
+    else:
+        await UserRegisterState.start_register.set()
+        await message.answer(
+            f'Xurmatli {message.from_user.full_name} siz ro\'yxatdan o\'tmagansiz !\n'
+            f'Botdan foydalanish uchun ro\'yxatdan o\'ting.',
+            reply_markup=buttons([_('register')])
+        )
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -15,7 +15,7 @@ async def bot_start(message: Message, state: FSMContext):
     full_name = await db.get_user_full_name(message.from_user.id)
     if full_name is not None:
         await message.answer(
-            f'🙋‍♂ Salom {full_name} yana ko\'rishganimizdan xursandman!',
+            f'🙋‍♂ Salom {full_name} yana ko\'rishganimizdan xursandman !',
             reply_markup=buttons([_('add_device'), _('my_devices')])
         )
     else:
